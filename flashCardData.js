545f471db8e1e9a55f9a2e819bf5c42ee2ed3903
Annessa Bock-Willmes <annessa.hartey@hotmail.com>
2019-04-29 14:55:22 +0000

    finished flashback

---
+++ flashCardData.js
@@ -0,0 +1,77 @@
+var importedFlashCardDataFile = [{
+    front: {
+      question: 'You may know that React was created at _________.',
+      hint: 'This company\'s site is known for being written in PHP.'
+    },
+    back: {
+      image: './images/Facebook.jpg',
+      answer: 'Facebook',
+      description: 'An engineer at the company negotiated with his manager to try a 6-month experiment in React.'
+    }
+  },
+  {
+    front: {
+      question: 'React started as a JavaScript port of _________.',
+      hint: 'This library is an HTML component framework for an extension of PHP.'
+    },
+    back: {
+      image: './images/XHP.png',
+      answer: 'XHP',
+      description: 'The concept of React was born from Angular and XHP.'
+    }
+  },
+  {
+    front: {
+      question: 'React was created by _________.',
+      hint: 'This software engineer wanted to experiment with taking XHP in-browser with JavaScript.'
+    },
+    back: {
+      image: './images/JordanWalke.png',
+      answer: 'Jordan Walke',
+      description: 'At the time, he was working on Facebook Ads. He later moved to Product Infrastructure to focus on React full-time.'
+    }
+  },
+  {
+    front: {
+      question: 'React\'s first deploy was on Facebook\'s _________ in 2011.',
+      hint: 'It was React that made \'liking\' your friends\' posts possible.'
+    },
+    back: {
+      image: './images/NewsFeed.png',
+      answer: 'News Feed',
+      description: 'The first version of Instagram\'s feed was also built using React. They were the first \'external\' user of React.'
+    }
+  },
+  {
+    front: {
+      question: 'At JSConf US in May 2013, React was _________.',
+      hint: 'Today, websites like Netflix, Airbnb, The New York Times, Tesla, Instagram, and more use React.'
+    },
+    back: {
+      image: './images/Open-Sourced.jpg',
+      answer: 'open-sourced',
+      description: 'React is maintained by Instagram, Facebook, and a community of individual developers and corporations.'
+    }
+  },
+  {
+    front: {
+      question: 'React allows you to break complex user interfaces into _________.',
+      hint: 'This concept of React uses JSX, which has HTML-like syntax, to generate real HTML using JavaScript.'
+    },
+    back: {
+      image: './images/Components.jpg',
+      answer: 'components',
+      description: 'Components accept inputs called props and return outputs describing what should appear on the screen.'
+    }
+  },
+  {
+    front: {
+      question: 'A _________ holds the data that can change during interaction with the application.',
+      hint: 'Whenever this changes, React calls the render method and applies any changes to the HTML of the page.'
+    },
+    back: {
+      image: './images/State.jpg',
+      answer: 'state',
+      description: 'State makes it simple and easy to organize aspects of the web application that change.'
+    }
+  }];
